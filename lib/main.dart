7afe71f12296fcd59358f6f1e891abca7a7a1c9a
emre <altunemre6@gmail.com>
2020-11-29 22:00:36 +0300

    project created

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_burc_rehberi/burc_liste.dart';
+
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Burç Rehberi",
+      theme: ThemeData(
+        primarySwatch: Colors.teal[400]
+
+      ),
+      home: BurcListesi(),
+    );
+  }
+
+}
